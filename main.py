5f480aaae30770fe49b13a350b864784f7c12d2c
whoisibk <oyetimehinjoseph@gmail.com>
2025-12-09 23:07:17 +0100

    Modularizing code

---
+++ main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+from stats_route import router as stats_router
+
+api = FastAPI()
+api.include_router(stats_router)
+
+total_requests, test1_requests, test2_requests  = 0, 0, 0
+
+@api.get("/test1")
+def test1():
+    global total_requests, test1_requests
+
+    total_requests += 1
+    test1_requests += 1
+    return {"This is the first test!"}
+
+
+@api.get("/test2")
+def test2():
+    global total_requests, test2_requests
+
+    total_requests += 1
+    test2_requests += 1 
+    return {"This is the second test!"}
---
+++ metrics.py
@@ -0,0 +1,2 @@
+from main import total_requests, test1_requests, test2_requests
+
