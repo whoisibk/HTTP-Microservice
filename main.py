b5528d35807d8a245579f1064ecac081d7b2c3e4
whoisibk <oyetimehinjoseph@gmail.com>
2025-12-10 10:42:11 +0100

    Venv changes

--- main.py
+++ main.py
@@ -6,34 +6,39 @@ api = FastAPI()
 
 total_requests, test1_requests, test2_requests, test3_requests = 0, 0, 0, 0
 
+client_host = ""
+
 
 @api.get("/test1", response_model=dict)
 def test1(request: Request):
-    global total_requests, test1_requests
+    global total_requests, test1_requests, client_host
 
     client_host = request.client.host
     total_requests += 1
     test1_requests += 1
+
     return {"This is the first test!"}
 
 
 @api.get("/test2", response_model=dict)
 def test2(request: Request):
-    global total_requests, test2_requests
+    global total_requests, test2_requests, client_host
 
-    client = request.client.host
+    client_host = request.client.host
     total_requests += 1
     test2_requests += 1
+
     return {"This is the second test!"}
 
 
 @api.get("/test3", response_model=dict)
 def test3(request: Request):
-    global total_requests, test3_requests
+    global total_requests, test3_requests, client_host
 
-    client = request.client.host
+    client_host = request.client.host
     total_requests += 1
     test3_requests += 1
+    
     return {"This is the third test!"}
 
 
--- metrics.py
+++ metrics.py
@@ -1,8 +1,37 @@
-from main import total_requests, test1_requests, test2_requests, test3_requests
+from time import time
+from datetime import datetime
+from main import (
+    total_requests,
+    test1_requests,
+    test2_requests,
+    test3_requests,
+    client_host,
+)
+
+max_tokens = 5
+token_refill_rate = 1  # tokens per second
+
 
 metrics = {
     "Total Requests Made": total_requests,
     "Test 1 Requests": test1_requests,
     "Test 2 Requests": test2_requests,
-    "Test 3 Requests": test3_requests
+    "Test 3 Requests": test3_requests,
+}
+
+current_time = time()
+
+buckets = {
+
+    }
+
+if client_host not in buckets:
+    buckets[client_host] = {
+        # "Total Requests Made": total_requests,
+        # "Test 1 Requests": test1_requests,
+        # "Test 2 Requests": test2_requests,
+        # "Test 3 Requests": test3_requests,
+        "tokens": 1,
+        "last_refill_time": datetime.strptime(time())
+
     }
